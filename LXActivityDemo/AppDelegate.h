22bc49fd1609abdc69b00640472b43bb383f2448
lcolco <452440967@qq.com>
2014-03-17 10:46:45 +0800

    Initial Commit

---
+++ LXActivityDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LXActivityDemo
+//
+//  Created by lixiang on 14-3-17.
+//  Copyright (c) 2014年 iMyQuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
